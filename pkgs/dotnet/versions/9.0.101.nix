947547d1e0e8afc0a72254be85df27eb62243aab
Konrad Malik <konrad.malik@gmail.com>
2024-12-05 12:02:26 +0100

    feat: dotnet 9.0.101

---
+++ pkgs/dotnet/versions/9.0.101.nix
@@ -0,0 +1,18 @@
+{
+  x86_64-linux = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/9.0.101/dotnet-sdk-9.0.101-linux-x64.tar.gz";
+    sha256 = "0manvpq7d6bzc8ff0x147hmj19i7a43iswl7x6z3d6f9rkpa7z62";
+  };
+  aarch64-linux = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/9.0.101/dotnet-sdk-9.0.101-linux-arm64.tar.gz";
+    sha256 = "0xrfpfyc25aj60maqzijkz1gqhm0szr1dqaji8qxcn8hag0i210n";
+  };
+  x86_64-darwin = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/9.0.101/dotnet-sdk-9.0.101-osx-x64.tar.gz";
+    sha256 = "0xzsji31d56z2l78ybagdds1vx8s1vmxqkjv9xsskaxfrr6h3z44";
+  };
+  aarch64-darwin = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/9.0.101/dotnet-sdk-9.0.101-osx-arm64.tar.gz";
+    sha256 = "0v7d87lxm9ch06qn6r5znbbqaarqss013z3rm13qrxpm4l97xbq2";
+  };
+}
