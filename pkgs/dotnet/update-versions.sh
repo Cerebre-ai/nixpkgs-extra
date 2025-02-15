ea12e1d94cbb26dd103e778cb50c150ece617b51
Konrad Malik <konrad.malik@gmail.com>
2025-02-15 13:24:02 +0100

    feat: add script and ci to automatically add missing dotnet versions

---
+++ pkgs/dotnet/update-versions.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# run from the main repo dir
+
+dotnet_versions_url="https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json"
+latest_versions=$(curl -s "$dotnet_versions_url" | jq -r '."releases-index"[] | select(."support-phase" != "eol") | ."latest-sdk"')
+check_dir="./pkgs/dotnet/versions"
+
+# Iterate through latest versions and check if the file exists
+for version in $latest_versions; do
+    filename="$check_dir/$version.nix"
+    if [[ -f "$filename" ]]; then
+        echo "File $filename does exist. Skipping $version."
+    else
+        echo "File $filename does not exist. Adding $version."
+        ./pkgs/dotnet/nix-prefetch-dotnet.sh "$version"
+    fi
+done
