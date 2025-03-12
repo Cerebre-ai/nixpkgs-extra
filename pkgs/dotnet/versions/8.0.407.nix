a540cb4a9723680a3efbea624527cea2b6098520
konradmalik <13033392+konradmalik@users.noreply.github.com>
2025-03-12 09:05:46 +0000

    feat: dotnet update

---
+++ pkgs/dotnet/versions/8.0.407.nix
@@ -0,0 +1,18 @@
+{
+  x86_64-linux = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/8.0.407/dotnet-sdk-8.0.407-linux-x64.tar.gz";
+    sha256 = "0zwwjczb63i5y18c18lxgcssa8hlzn676b6vamynl3ly8ns2s4lr";
+  };
+  aarch64-linux = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/8.0.407/dotnet-sdk-8.0.407-linux-arm64.tar.gz";
+    sha256 = "1gdzcr27zka5fb8368847yf06qlczc71chlwk37y57j52sy3zxh2";
+  };
+  x86_64-darwin = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/8.0.407/dotnet-sdk-8.0.407-osx-x64.tar.gz";
+    sha256 = "09wp8vxb19x1nbd1mfhawlz0isyikjgv1p01k3s0619bdx0zgi4q";
+  };
+  aarch64-darwin = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/8.0.407/dotnet-sdk-8.0.407-osx-arm64.tar.gz";
+    sha256 = "0fnf9znsm4mfv6zznd9g7af4d1ddgjvgcv16cmwds3fwvkbdd1kh";
+  };
+}
