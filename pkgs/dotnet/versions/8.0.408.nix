2bf8d478964406354ada1576b7232545e9e1e249
konradmalik <13033392+konradmalik@users.noreply.github.com>
2025-04-10 08:34:18 +0000

    feat: dotnet update

---
+++ pkgs/dotnet/versions/8.0.408.nix
@@ -0,0 +1,18 @@
+{
+  x86_64-linux = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/8.0.408/dotnet-sdk-8.0.408-linux-x64.tar.gz";
+    sha256 = "16z32dqj0clj5a34icffxsn1fdikn30yqf9dqhhg5p73nyswz39g";
+  };
+  aarch64-linux = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/8.0.408/dotnet-sdk-8.0.408-linux-arm64.tar.gz";
+    sha256 = "0wl21wb6apqh0vajafk086awssshsa0av0bpra58iqxspg3halwf";
+  };
+  x86_64-darwin = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/8.0.408/dotnet-sdk-8.0.408-osx-x64.tar.gz";
+    sha256 = "05hy23gd0n7gq3anl67yl1crr9hy0q1qs3iqfpim8wzsa70yydws";
+  };
+  aarch64-darwin = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/8.0.408/dotnet-sdk-8.0.408-osx-arm64.tar.gz";
+    sha256 = "09dv76hy72xz8ka6952bb9646h9wxgd9x8vjxl3g035bffvn0avg";
+  };
+}
---
+++ pkgs/dotnet/versions/9.0.203.nix
@@ -0,0 +1,18 @@
+{
+  x86_64-linux = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/9.0.203/dotnet-sdk-9.0.203-linux-x64.tar.gz";
+    sha256 = "18r3vvk7cyb1qphnc0ya1nrqnirkf6f1appc54dg6x52c009psf7";
+  };
+  aarch64-linux = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/9.0.203/dotnet-sdk-9.0.203-linux-arm64.tar.gz";
+    sha256 = "16jq9n1a79inzhpqzviq2l4pvam5qdr3kdvk1isczyhsr0sjpy2r";
+  };
+  x86_64-darwin = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/9.0.203/dotnet-sdk-9.0.203-osx-x64.tar.gz";
+    sha256 = "0nh6zgp85ch54cl7hqxhqzg9awgaz55yxsdzffxgab57vqyvl7qn";
+  };
+  aarch64-darwin = {
+    url = "https://dotnetcli.azureedge.net/dotnet/Sdk/9.0.203/dotnet-sdk-9.0.203-osx-arm64.tar.gz";
+    sha256 = "1ly6yq53mlvhp8hp1a9wdf786j0cmd6s7pw4fn5bcwlmj4ngcn4m";
+  };
+}
